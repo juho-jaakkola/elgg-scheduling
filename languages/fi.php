8fb00127271e04c3090c8e61f3faea90c9781ff7
Juho Jaakkola <juho.jaakkola@mediamaisteri.com>
2015-04-07 09:53:19 +0300

    Pulled Finnish translations from Transifex

---
+++ languages/fi.php
@@ -0,0 +1,20 @@
+<?php
+
+return array(
+	'scheduling' => 'Aikataulutus',
+	'scheduling:add' => 'Lisää uusi kysely',
+	'item:object:scheduling_poll' => 'Aikataulukysely',
+	'item:object:scheduling_poll_slot' => 'Aikataulukyselyn vaihtoehto',
+	'scheduling:date_format' => 'D j.n.Y',
+	'scheduling:edit:time' => 'Muokkaa aikoja',
+	'scheduling:slot:title' => 'Aika %s',
+	'scheduling:column:add' => 'Lisää sarake',
+	'scheduling:error:invalid_format' => '"%s" on virheellinen aika',
+	'scheduling:add_day' => 'Lisää päivä',
+
+	'scheduling:save:success' => 'Kysely tallennettu',
+	'scheduling:save:error' => 'Jotakin meni pieleen. Tarkista, että syöttämäsi tiedot ovat oikein.',
+	'scheduling:error:cannot_edit' => 'Sinulla ei ole oikeuksia tämän kyselyn muokkaamiseen',
+	'scheduling:delete:success' => 'Kysely poistettu',
+	'scheduling:delete:error' => 'Kyselyn poistaminen epäonnistui',
+);
