bef34814cc3190077543682f9317877e357a41c9
Juho Jaakkola <juho@elgg.org>
2015-11-22 14:21:57 +0200

    chore(i18n): updates translations

--- languages/fi.php
+++ languages/fi.php
@@ -9,12 +9,31 @@ return array(
 	'scheduling:edit:time' => 'Muokkaa aikoja',
 	'scheduling:slot:title' => 'Aika %s',
 	'scheduling:column:add' => 'Lisää sarake',
-	'scheduling:error:invalid_format' => 'Kellonajan täytyy olla muodossa HH:MM',
+	'scheduling:row:copy' => 'Kopioi rivi',
+	'scheduling:row:delete' => 'Poista rivi',
+	'scheduling:error:invalid_format' => 'Ajan pitää olla muodossa HH:MM',
 	'scheduling:add_day' => 'Lisää päivä',
+	'scheduling:group:enable' => 'Ota käyttöön ryhmän aikataulutyökalu',
+
+	'scheduling:owner_block' => 'Aikataulutus',
+	'scheduling:owner_block:group' => 'Ryhmän aikataulutus',
 
 	'scheduling:save:success' => 'Kysely tallennettu',
 	'scheduling:save:error' => 'Jotakin meni pieleen. Tarkista, että syöttämäsi tiedot ovat oikein.',
 	'scheduling:error:cannot_edit' => 'Sinulla ei ole oikeuksia tämän kyselyn muokkaamiseen',
 	'scheduling:delete:success' => 'Kysely poistettu',
 	'scheduling:delete:error' => 'Kyselyn poistaminen epäonnistui',
+
+	// Notifications
+	'scheduling:notify:publish:subject' => 'Uusi aikataulukysely',
+	'scheduling:notify:publish:body' => '%s lisäsi uuden aikataulukyselyn: "%s"
+
+%s',
+	'scheduling:notify:publish:summary' => '%s lisäsi uuden aikataulukyselyn: "%s"',
+	'scheduling:notify:update:subject' => 'Päivittynyt aikataulukysely',
+	'scheduling:notify:update:body' => '%s on päivittänyt kyselyä "%s", johon olet vastannut aiemmin.
+
+Tarkista, vaikuttavatko muutokset aikaisemman antamaasi vastaukseen:
+%s',
+	'scheduling:notify:update:summary' => '%s on päivittänyt kyselyä: %s',
 );
