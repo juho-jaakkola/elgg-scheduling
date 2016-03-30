e55c06547dfb96c002ed0eea745a0b51beb2364f
Juho Jaakkola <juho.jaakkola@mediamaisteri.com>
2016-03-30 09:03:48 +0300

    feature(i18n): Adds Italian translation

---
+++ languages/it.php
@@ -0,0 +1,36 @@
+<?php
+
+return array (
+	'scheduling:date_format' => 'D j/n/Y',
+	'scheduling' => 'Programmazione',
+	'scheduling:add' => 'Nuovo sondaggio',
+	'item:object:scheduling_poll' => 'Sondaggi per le programmazioni',
+	'item:object:scheduling_poll_slot' => 'Orari del sondaggio per la programmazione',
+	'scheduling:edit:time' => 'Modifica gli orari',
+	'scheduling:slot:title' => 'Orario %s',
+	'scheduling:column:add' => 'Aggiungi colonna',
+	'scheduling:row:copy' => 'Duplica riga',
+	'scheduling:row:delete' => 'Elimina riga',
+	'scheduling:error:invalid_format' => "L'ora deve essere nel formato OO:MM",
+	'scheduling:add_day' => 'Aggiungi un giorno',
+	'scheduling:group:enable' => 'Abilita lo strumento programmazione per il gruppo',
+	'scheduling:owner_block' => 'Programmazione',
+	'scheduling:owner_block:group' => 'Programmazione del gruppo',
+	'scheduling:save:success' => 'Sondaggio per la programmazione salvato',
+	'scheduling:save:error' => 'Qualcosa non ha funzionato. per favore verifica che i valori inseriti siano validi.',
+	'scheduling:error:cannot_edit' => 'Non sei autorizzato/a a modificare questo contenuto',
+	'scheduling:delete:success' => 'Sondaggio per la programmazione eliminato',
+	'scheduling:delete:error' => 'Eliminazione sondaggio non riuscita',
+	'scheduling:notify:publish:body' => '%s ha creato un nuovo sondaggio per la programmazione: "%s"
+
+%s',
+	'scheduling:notify:publish:summary' => '%s ha creato un nuovo sondaggio per la programmazione: %s',
+	'scheduling:notify:update:subject' => 'Sondaggio per la programmazione aggiornato',
+	'scheduling:notify:update:body' => '%s ha aggiornato il sondaggio per la programmazione "%s" a cui hai già risposto.
+
+Verifica se la tua risposta deve essere modificata:
+%s',
+	'scheduling:notify:publish:subject' => 'Nuovo sondaggio per la programmazione',
+	'scheduling:notify:update:summary' => '%s ha aggiornato il sondaggio per la programmazione: %s',
+	'river:create:object:scheduling_poll' => '%s ha creato il nuovo sondaggio per la programmazione %s',
+);
