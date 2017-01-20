9d5eba70b52071b97fdb38edc02cc74f1df7307f
bm jade <benoitmottin@ymail.com>
2017-01-20 11:56:59 +0100

    ADD 	page with the days selection in a plain datepicker 	create days widthout slots 	Slot page + action

--- actions/scheduling/days.php
+++ actions/scheduling/days.php
@@ -2,6 +2,8 @@
 
 elgg_make_sticky_form('scheduling');
 
+
+
 $guid = get_input('guid');
 $entity = get_entity($guid);
 
@@ -11,28 +13,37 @@ if (!$entity instanceof ElggSchedulingPoll || !$entity->canEdit()) {
 }
 
 $slots = array();
-$input = (array) get_input('slots', array());
+$input = (array) get_input('poll-days', array());
 
 // type of poll, simple or advanced (2 or 3 answer possible)
-$pollType = get_input('pollType', false);
-
-if ($pollType == 'on') {
-    $entity->setPollType(PollType::ADVANCE);
-} else {
-    $entity->setPollType(PollType::SIMPLE);
-}
-
+//$pollType = get_input('pollType', false);
+/*
+  if ($pollType == 'on') {
+  $entity->setPollType(PollType::ADVANCE);
+  } else {
+  $entity->setPollType(PollType::SIMPLE);
+  }
+  // */
 foreach ($input as $index => $date_info) {
-
-    $date = $date_info['date'];
-    $date_slots = $date_info['slot'];
-    foreach ($date_slots as $slot) {
-        if (empty($slot)) {
-            continue;
-        }
-        $slots[] = strtotime("$date $slot");
-    }
+    elgg_dump("-----input-------");
+    elgg_dump($input);
+    elgg_dump("---------------------");
+    
+    
+    /*
+      $date = $date_info['date'];
+      $date_slots = $date_info['slot'];
+      foreach ($date_slots as $slot) {
+      /*if (empty($slot)) {
+      continue;
+      }// */
+    $slots[] = strtotime("$date_info");
+    
+    elgg_dump("-----slot-------");
+    elgg_dump($slots);
+    elgg_dump("---------------------");
 }
+
 
 if ($entity->setSlots($slots)) {
     system_message(elgg_echo('scheduling:save:success'));
